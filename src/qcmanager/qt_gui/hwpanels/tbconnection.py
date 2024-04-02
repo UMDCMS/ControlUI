6e91657da50e57c64b8b6772d9a6970550ad5239
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-04-02 12:19:10 +0200

    Changed the particle ordering

--- src/qcmanager/qt_gui/hwpanels/tbconnection.py
+++ src/qcmanager/qt_gui/hwpanels/tbconnection.py
@@ -52,8 +52,8 @@ class TBConnectionPanel(_QContainer):
         self.__init_layout__()
         self._display_update()
         self.connect_button.run_connect(self.tb_connect)
-        self.discard_button.clicked.connect(self.tb_clear)
         self.disconnect_button.run_connect(self.tb_disconnect)
+        self.discard_button.clicked.connect(self.tb_clear)
         self.update_timer.timeout.connect(self.update_plot_data)
         self.update_timer.start()
 
